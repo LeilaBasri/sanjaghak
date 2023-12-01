0eb52d8777f179fff64eb5db707acf7851353b59
Leila Basri <leilabasrilb@gmail.com>
2023-11-30 21:41:09 -0800

    Update

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,13 @@
-
+$(document).ready(function () {
+    $(".open-cart").click(function () {
+        $(".open-cart").toggleClass("active-cart");
+        $(".cart").fadeToggle();
+    })
+    $(".sale-timeout-counter").startTimer();
+    $('.selectpicker').selectpicker({
+        size: 10
+    });
+    $("#owl-example").owlCarousel();
+    $(".fancybox").fancybox();
+    $(".zoom").elevateZoom();
+})
--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+$(document).ready(function () {
 
+    $('.nav ul li ul li').hover(
+        function () {
+
+            $('.nav ul li ul li ul').css("display", "none");
+            $('ul', this).css("display", "block");
+
+        }
+    );
+
+});
